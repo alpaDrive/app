3d438877ab480613b3946b186bfd730cd2b696b6
joeljohnson21 <joeljohnson0007@gmail.com>
2023-05-17 20:04:52 +0530

    Implemented progress bar

--- src/screens/Connect/Connect.js
+++ src/screens/Connect/Connect.js
@@ -1,24 +1,62 @@
-import { View, Text } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { MaterialIcons } from '@expo/vector-icons';
-import styles from'./styles';
+import ProgressCircle from 'react-native-progress-circle'
+import styles from './styles';
 
 const Connect = () => {
-   
-    return (
-    <SafeAreaView style={styles.container}>
+  const [fuelPercentage, setFuelPercentage] = useState(60);
+  const [gear, setGear] = useState(3);
+  const [speed, setSpeed] = useState(45);
+  const [rpm, setRpm] = useState(70);
+  const [coolTemp, setcoolTemp] = useState(38);
 
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [text1, setText1] = useState('43,500');
+  const [text2, setText2] = useState('Total');
+  const [isLeftArrowVisible, setIsLeftArrowVisible] = useState(true);
+  const [isRightArrowVisible, setIsRightArrowVisible] = useState(true);
+
+  const handleLeftArrowClick = () => {
+    if (!isFlipped) {
+      setText1('23');
+      setText2('Trip A');
+      setIsLeftArrowVisible(false);
+    } else {
+      setText1('43,500');
+      setText2('Total');
+      setIsRightArrowVisible(true);
+    }
+    setIsFlipped(!isFlipped);
+  };
+
+  const handleRightArrowClick = () => {
+    if (!isFlipped) {
+      setText1('57');
+      setText2('Trip B');
+      setIsRightArrowVisible(false);
+      setIsLeftArrowVisible(true);
+    } else {
+      setText1('43,500');
+      setText2('Total');
+      setIsLeftArrowVisible(true);
+    }
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
       <View style={styles.titleview}>
         <View style={styles.titleflex}><Text style={styles.title}>ALPADRIVE</Text></View>
         <View style={styles.titleempty}></View>
         <View style={styles.iconflex}>
-        <Feather name="user" size={29} color="white" />
+          <Feather name="user" size={29} color="white" />
         </View>
       </View>
-
       <View style={styles.gearview}>
         <View style={styles.endflex}></View>
         <View style={styles.gearflex}>
@@ -28,64 +66,96 @@ const Connect = () => {
           </View>
           <View style={styles.gearempty}></View>
           <View style={styles.gearpos}>
-            <View style={styles.gearno}><Text style={styles.gearnotext}>3</Text></View>
+            <View style={styles.gearno}><Text style={styles.gearnotext}>{gear}</Text></View>
             <View style={styles.gear}><Text style={styles.geartext}>Gear</Text></View>
           </View>
         </View>
         <View style={styles.endflex}></View>
       </View>
-
       <View style={styles.speedview}>
-
         <View style={styles.upper}>
           <View style={styles.rpmempty}></View>
           <View style={styles.speedflex}>
-            <View style={styles.speedcom}>
-              <Text style={styles.speedtext}>45</Text>
-              <Text style={styles.kilometer}>km/h</Text>
+            <View style={styles.speedometer}>
+              <ProgressCircle
+                percent={speed}
+                radius={90}
+                borderWidth={9}
+                color="white"
+                shadowColor="#00000D"
+                bgColor="#051128"
+                borderRadius={20}>
+                <Text style={styles.kilometer}>km/h</Text>
+                <Text style={styles.speedcom}>{speed}</Text>
+              </ProgressCircle>
             </View>
           </View>
           <View style={styles.tempempty}></View>
-          </View>
-
+        </View>
         <View style={styles.lower}>
           <View style={styles.rpmflex}>
-            <View style={styles.rpmcom}>
-              <Text style={styles.rpm}>3000</Text>
-              <Text style={styles.rpmtext}>RPM</Text>
+            <View style={styles.rpmtemp}>
+              <ProgressCircle
+                percent={rpm}
+                radius={50}
+                borderWidth={5}
+                color="#1559DC"
+                shadowColor="#00000D"
+                bgColor="#051128"
+                borderRadius={20}>
+                <Text style={styles.rpmtext}>RPM</Text>
+                <Text style={styles.rpmcom}>{rpm}</Text>
+              </ProgressCircle>
             </View>
           </View>
           <View style={styles.speedempty}></View>
           <View style={styles.tempflex}>
-            <View style={styles.tempcom}>
-            <MaterialCommunityIcons name="coolant-temperature" size={32} color="white" />
+            <View style={styles.rpmtemp}>
+              <ProgressCircle
+                percent={coolTemp}
+                radius={50}
+                borderWidth={5}
+                color="#1559DC"
+                shadowColor="#00000D"
+                bgColor="#051128"
+                borderRadius={20}>
+                <View style={styles.tempcom}>
+                  <MaterialCommunityIcons name="coolant-temperature" size={32} color="white" />
+                </View>
+              </ProgressCircle>
             </View>
-          </View>
-          </View>  
-      </View>
-
-      <View style={styles.fuelview}>
-        <View style={styles.fuelflex}><Text style={styles.fueltext}>
-        <FontAwesome5 name="gas-pump" size={20} color="white" />
-          <Text style={styles.subtext}>  Fuel</Text> (estimated 250km remaining)<Text style={styles.subtext}> 60%</Text></Text></View>
-        <View style={styles.fuelmeter}>
-          <View style={styles.meterout}>
-            <View style={styles.meterin}></View>
           </View>
         </View>
       </View>
-
+      <View style={styles.fuelview}>
+        <View style={styles.fuelflex}><Text style={styles.fueltext}>
+          <FontAwesome5 name="gas-pump" size={20} color="white" />
+          <Text style={styles.subtext}>  Fuel</Text> (estimated 250km remaining) <Text style={styles.subtext}> {fuelPercentage}%</Text></Text></View>
+        <View style={styles.fuelmeter}>
+          <View style={styles.meterout}>
+            <View style={[styles.meterin, { width: `${fuelPercentage}%` }]}></View>
+          </View>
+        </View>
+      </View>
       <View style={styles.odoview}>
         <View style={styles.arrowflex}>
-        <MaterialIcons name="keyboard-arrow-left" size={30} color="white" />
+          {isLeftArrowVisible && (
+            <TouchableOpacity style={styles.button} onPress={handleLeftArrowClick}>
+              <MaterialIcons name="keyboard-arrow-left" size={30} color="white" />
+            </TouchableOpacity>
+          )}
         </View>
         <View style={styles.odoflex}>
           <View style={styles.odocom}>
-            <Text style={styles.odono}>49,950 km</Text>
-            <Text style={styles.odotext}>Total</Text></View>
+            <Text style={styles.odono}>{text1} km</Text>
+            <Text style={styles.odotext}>{text2}</Text></View>
         </View>
         <View style={styles.arrowflex}>
-        <MaterialIcons name="keyboard-arrow-right" size={30} color="white" />
+          {isRightArrowVisible && (
+            <TouchableOpacity style={styles.button} onPress={handleRightArrowClick}>
+              <MaterialIcons name="keyboard-arrow-right" size={30} color="white" />
+            </TouchableOpacity>
+          )}
         </View>
       </View>
       <View style={styles.statview}>
@@ -95,12 +165,10 @@ const Connect = () => {
             <Text style={styles.periodic}>Periodic stats, health report</Text>
           </View>
           <View style={styles.statarrow}>
-          <MaterialIcons name="keyboard-arrow-right" size={30} color="black" />
+            <MaterialIcons name="keyboard-arrow-right" size={30} color="black" />
           </View>
         </View>
       </View>
-
-           
     </SafeAreaView>
   );
 };
