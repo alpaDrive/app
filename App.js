243bae3f1826efae4a5e290efe65600f79d3cb54
Vishal <321vishalds@gmail.com>
2023-05-19 11:28:31 +0530

    Finished UI part 2
    * Finished console UI
    
    * Implemented progress bar
    
    Co-authored-by: joeljohnson21 <joeljohnson0007@gmail.com>

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ques from './src/screens/Ques/Ques';
+import Connect from './src/screens/Connect/Connect';
 // import Home from './src/screens/Home/Home'
 import Empty from './src/screens/Empty/Empty';
 import Pairing from './src/screens/Pairing/Pairing';
@@ -9,20 +11,20 @@ import Pairing from './src/screens/Pairing/Pairing';
 // import Periodic from './src/screens/Periodic/Periodic';
 // import Overall from './src/screens/Overall/Overall'
 
-
-
 const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name='Connect' component={Connect} options={{ headerShown: false }} />
+        <Stack.Screen name='Ques' component={Ques} options={{ headerShown: false }} />
         <Stack.Screen name='Empty' component={Empty} options={{ headerShown: false }} />
         <Stack.Screen name='Pairing' component={Pairing} options={{ headerShown: false }} />
-        {/* <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} /> */}
-        {/* <Stack.Screen name='Daily' component={Daily} options={{ headerShown: false }} />
+        <Stack.Screen name='Home' component={Home} options={{ headerShown: false }} />
+        <Stack.Screen name='Daily' component={Daily} options={{ headerShown: false }} />
         <Stack.Screen name='Periodic' component={Periodic} options={{ headerShown: false }} />
-        <Stack.Screen name='Overall' component={Overall} options={{ headerShown: false }} />   */}
+        <Stack.Screen name='Overall' component={Overall} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
