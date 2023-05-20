7562d28dafef0c5124301c223b8cf6da553e2d51
vishalkrishnads <321vishalds@gmail.com>
2023-05-20 11:22:09 +0530

    Built pairing screen

--- src/screens/Pairing/Pairing.js
+++ src/screens/Pairing/Pairing.js
@@ -3,23 +3,29 @@ import { SafeAreaView, View, Text, TouchableOpacity, Image, TextInput, Alert } f
 import { Vibration } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { Camera } from 'expo-camera';
+import * as SecureStore from 'expo-secure-store';
 import Modal from 'react-native-modal';
+import configs from '../../assets/configs';
 import styles from './styles';
 
-const Pairing = () => {
-    const [ flash, setFlash ] = useState(Camera.Constants.FlashMode.off);
-    const [ hasPermission, setHasPermission ] = useState(null);
-    const [ isQRCodeDetected, setIsQRCodeDetected ] = useState(false);
-    const [ isModalVisible, setIsModalVisible ] = useState(false);
-    const [ qrCodeData, setQRCodeData ] = useState(null);
-    const [ input1, setInput1 ] = useState('');
-    const [ input2, setInput2 ] = useState('');
+const Pairing = ({ navigation }) => {
+    const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+    const [hasPermission, setHasPermission] = useState(null);
+    const [isQRCodeDetected, setIsQRCodeDetected] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [input1, setInput1] = useState('');
+    const [input2, setInput2] = useState('');
     const cameraRef = React.useRef(null);
+
+    let uid = '', vehicles = []
 
     useEffect(() => {
         (async () => {
             const { status } = await Camera.requestCameraPermissionsAsync();
             setHasPermission(status === 'granted');
+
+            uid = JSON.parse(await SecureStore.getItemAsync("alpaDrive-user")).uid.$oid;
+            vehicles = JSON.parse(await SecureStore.getItemAsync("alpaDrive-vehicles"));
         })();
     }, []);
 
@@ -41,123 +47,154 @@ const Pairing = () => {
     const handleModalClose = () => {
         setIsModalVisible(false);
         setIsQRCodeDetected(false);
-        setQRCodeData(null);
     };
 
-    const handleSend = () => {
-        if (!input1 || !input2) {
+    const handleSend = async () => {
+        if (!input1 || !input2)
             Alert.alert('Oops', "You can't leave a field blank!");
-        } else {
-            const dataToSend = {
-                input1,
-                input2,
-            };
-            // Send the data
+        else {
+            const response = await fetch(`https://${configs.SERVER_URL}/vehicle/edit`, {
+                method: 'POST',
+                headers: new Headers(),
+                body: JSON.stringify({
+                    "vid": vid,
+                    "company": input2,
+                    "model": input1
+                })
+            })
+            const body = await response.json()
+            if (response.ok) Alert.alert(
+                'Welcome aboard!',
+                `Your new ${body.document.company} ${body.document.model} has been paired and is ready to go. Open the app to see what you can do with alpaDrive.`,
+                [{
+                    text: 'Show Me',
+                    onPress: () => navigation.navigate('Home'),
+                    style: 'default'
+                }]
+            )
+            else Alert.alert('Uh oh!', JSON.stringify(body.error))
         }
     };
 
-    useEffect(() => {
-        if (isQRCodeDetected && qrCodeData) {
-            try {
-                const parsedData = JSON.parse(qrCodeData);
-
-                // Check if the parsed data contains a valid vehicle ID (vid) and initial value
-                if (parsedData && parsedData.vid && parsedData.initial) {
-                    setIsModalVisible(true); // Show the modal
-                    Vibration.vibrate(); // Vibrate when QR code is detected
-                } else {
-                    Alert.alert('Error', 'Invalid QR code');
-                }
-            } catch (error) {
-                Alert.alert('Error', 'Invalid QR code');
+    const handleBarCodeScanned = async ({ data }) => {
+        try {
+            const parsedData = JSON.parse(data);
+            if (parsedData.hasOwnProperty("vid") && parsedData.hasOwnProperty("initial")) {
+                Vibration.vibrate();
+                for (const each of vehicles)
+                    if (parsedData.vid === each._id.$oid)
+                        Alert.alert(
+                            'We got you!',
+                            'This vehicle has already been paired to your account. If you itend to pair this vehicle to another account, then just sign in to that account on the app, and show the QR code from your acccount',
+                            [
+                                {
+                                    text: 'Pair Another',
+                                    style: 'cancel'
+                                },
+                                {
+                                    text: 'Go Home',
+                                    onPress: () => navigation.navigate('Home'),
+                                    style: 'default'
+                                }
+                            ]
+                        )
+                const response = await fetch(`https://${configs.SERVER_URL}/pair/${parsedData.vid}/${uid}?initial=${parsedData.initial}`)
+                const body = await response.json()
+                if (response.ok) {
+                    if (parsedData.initial) { setIsModalVisible(true); vid = parsedData.vid; }
+                    else Alert.alert(
+                        'Welcome aboard!',
+                        'Your new vehicle has been paired and is ready to go. Open the app to see what you can do with alpaDrive.',
+                        [{
+                            text: 'Show Me',
+                            onPress: () => navigation.navigate('Home'),
+                            style: 'default'
+                        }]
+                    )
+                } else Alert.alert('Uh, oh!', JSON.stringify(body.error))
             }
-        }
-    }, [ isQRCodeDetected, qrCodeData ]);
-
-    const handleBarCodeScanned = ({ data }) => {
-        setIsQRCodeDetected(true);
-        setQRCodeData(data);
+        } catch { }
     };
 
     return (
-        <SafeAreaView style={ styles.rootp }>
-            <View style={ styles.headerp }>
-                <View style={ { flex: 0.5 } }></View>
-                <View style={ { flex: 1, flexDirection: 'row' } }>
-                    <View style={ { flex: 0.3 } }></View>
-                    <View style={ { flex: 3, justifyContent: 'center', alignItems: 'center' } }>
-                        <Image source={ require('../../assets/logo.png') } />
+        <SafeAreaView style={styles.rootp}>
+            <View style={styles.headerp}>
+                <View style={{ flex: 0.5 }}></View>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <View style={{ flex: 0.3 }}></View>
+                    <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
+                        <Image source={require('../../assets/logo.png')} />
                     </View>
-                    <View style={ { flex: 1, justifyContent: 'center', alignItems: 'center' } }>
-                        <TouchableOpacity onPress={ handleTorch }>
-                            { flash === Camera.Constants.FlashMode.off ? (
-                                <Ionicons name="flash-off-sharp" size={ 24 } color="white" />
+                    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                        <TouchableOpacity onPress={handleTorch}>
+                            {flash === Camera.Constants.FlashMode.off ? (
+                                <Ionicons name="flash-off-sharp" size={24} color="white" />
                             ) : (
-                                <Ionicons name="flash" size={ 24 } color="white" />
-                            ) }
+                                <Ionicons name="flash" size={24} color="white" />
+                            )}
                         </TouchableOpacity>
                     </View>
                 </View>
             </View>
-            { hasPermission ? (
+            {hasPermission ? (
                 <React.Fragment>
                     <Camera
-                        style={ styles.camera }
-                        type={ Camera.Constants.Type.back }
-                        flashMode={ flash }
-                        ref={ cameraRef }
+                        style={styles.camera}
+                        type={Camera.Constants.Type.back}
+                        flashMode={flash}
+                        ref={cameraRef}
                         ratio="16:9" // set the desired aspect ratio here
-                        rectOfInterest={ focusArea }
-                        onBarCodeScanned={ isQRCodeDetected ? undefined : handleBarCodeScanned }
+                        rectOfInterest={focusArea}
+                        onBarCodeScanned={isQRCodeDetected ? undefined : handleBarCodeScanned}
                     />
-                    { isQRCodeDetected && (
-                        <View style={ styles.vibrationOverlay }>
-                            {/* Customize the vibration effect overlay as needed */ }
-                            <Text style={ styles.vibrationText }>QR code detected</Text>
+                    {isQRCodeDetected && (
+                        <View style={styles.vibrationOverlay}>
+                            {/* Customize the vibration effect overlay as needed */}
+                            <Text style={styles.vibrationText}>QR code detected</Text>
                         </View>
-                    ) }
+                    )}
                 </React.Fragment>
             ) : (
                 <Text>No camera permission granted</Text>
-            ) }
-            <View style={ styles.bottom1 }>
-                <View style={ styles.text }>
-                    <Text style={ { color: 'white', justifyContent: 'center', alignItems: 'center' } }>
+            )}
+            <View style={styles.bottom1}>
+                <View style={styles.text}>
+                    <Text style={{ color: 'white', justifyContent: 'center', alignItems: 'center' }}>
                         Scan the QR code on your device
                     </Text>
                 </View>
             </View>
-            <Modal isVisible={ isModalVisible } animationIn="fadeIn" animationOut="fadeOut">
-                <View style={ styles.modalContainer_pairing }>
-                    <View style={ styles.modalContent_Pairing }>
-                        <View style={ { flex: 2, justifyContent: 'flex-end', alignItems: 'center' } }>
+            <Modal isVisible={isModalVisible} animationIn="fadeIn" animationOut="fadeOut">
+                <View style={styles.modalContainer_pairing}>
+                    <View style={styles.modalContent_Pairing}>
+                        <View style={{ flex: 2, justifyContent: 'flex-end', alignItems: 'center' }}>
                             <TextInput
-                                style={ styles.input }
-                                value={ input1 }
-                                onChangeText={ (text) => setInput1(text) }
+                                style={styles.input}
+                                value={input1}
+                                onChangeText={(text) => setInput1(text)}
                                 placeholder="Enter your car name"
                             />
                         </View>
-                        <View style={ { flex: 2, justifyContent: 'center', alignItems: 'center' } }>
+                        <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
                             <TextInput
-                                style={ styles.input }
-                                value={ input2 }
-                                onChangeText={ (text) => setInput2(text) }
+                                style={styles.input}
+                                value={input2}
+                                onChangeText={(text) => setInput2(text)}
                                 placeholder="Enter the brand name"
                             />
                         </View>
-                        <View style={ { flex: 1.5, flexDirection: 'row' } }>
+                        <View style={{ flex: 1.5, flexDirection: 'row' }}>
                             <TouchableOpacity
-                                style={ { flex: 1, justifyContent: 'center', alignItems: 'center' } }
-                                onPress={ handleModalClose }
+                                style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+                                onPress={handleModalClose}
                             >
-                                <Text style={ { color: 'white' } }>Close</Text>
+                                <Text style={{ color: 'white' }}>Close</Text>
                             </TouchableOpacity>
                             <TouchableOpacity
-                                style={ { flex: 1, justifyContent: 'center', alignItems: 'center' } }
-                                onPress={ handleSend }
+                                style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+                                onPress={handleSend}
                             >
-                                <Text style={ { color: 'white' } }>Submit</Text>
+                                <Text style={{ color: 'white' }}>Submit</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
